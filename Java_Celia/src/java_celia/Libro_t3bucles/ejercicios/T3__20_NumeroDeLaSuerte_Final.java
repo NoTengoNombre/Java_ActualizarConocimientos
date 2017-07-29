c1e60464d6abcbebe3f98bd38543b45aebf415dd
raulvela <siguelasreglas@gmail.com>
2017-07-29 12:57:29 +0200

    Ejercicio 20 Numero de la suerte

--- Java_Celia/src/java_celia/Libro_t3bucles/ejercicios/T3__20_NumeroDeLaSuerte_Final.java
+++ Java_Celia/src/java_celia/Libro_t3bucles/ejercicios/T3__20_NumeroDeLaSuerte_Final.java
@@ -15,7 +15,7 @@ public class T3__20_NumeroDeLaSuerte_Final {
     private static int dia;
     private static int mes;
     private static int anio;
-    private static int totalFecha;
+    private static int total;
 
     public static int dia() {
         Scanner sc = new Scanner(System.in);
@@ -65,123 +65,155 @@ public class T3__20_NumeroDeLaSuerte_Final {
         return anio;
     }
 
-    public static int dividirAnioGeneral(int anio) {
-        System.out.println("Anio : " + anio);
-        int d1 = anio / 1000;
-        int r1 = anio % 1000;
-        int d2 = r1 / 100;
-        int r2 = anio % 100;
-        int d3 = r2 / 10;
-        int r3 = anio % 10;
-        int d4 = r3 / 1;
-        anio = d1 + d2 + d3 + d4;
-        System.out.println("=========" + anio + "=========");
-        return anio; // devuelve la suma de todo el año
-    }
-
-    public static int dividir2cifrasAnio(int anio) {
-        System.out.println("--------- ANIO -----------");
-        int total2dividir;
-        int total3;
-        int total3_resto;
-        int total4;
-        int total_final;
-
-        total2dividir = anio;
-        total2dividir = total2dividir * 10;
-        total3 = total2dividir / 100;
-        total3_resto = total2dividir % 100;
-        total4 = total3_resto / 10;
-        total_final = total3 + total4;
-
-        System.out.println("Total : " + total_final);
-        if (total_final == 10) {
-            total_final = 1;
-        }
-        System.out.println("Anio unido en 1 cifra : " + total_final);
-        System.out.println("---------------------------------");
-
-        return total_final; // devuelve la suma del año de 2 cifras
-    }
-
-    public static int dividir2cifrasMes(int mes) {
-        System.out.println("--------- MES -----------");
-        int mes1 = 0;
-        int total1;
-        int total2;
-        int total3;
-        mes1 = mes * 10;
-        total1 = mes1 / 100;
-        int total_resto = mes1 % 100;
-        total2 = total_resto % 10;
-        total3 = total1 + total2;
-        return total3; // devuelve el mes en 1 cifra
-    }
-
+    /**
+     * Devuelve el dia en un solo numero
+     *
+     * @param dia
+     * @return
+     */
     public static int dividir2cifrasDia(int dia) {
-        System.out.println("........... DIA ................");
+        System.out.println("----------- DIA ---------------");
         int dia2;
         int total1;
         int total2;
         int total3;
+
         dia2 = dia * 10;
         total1 = dia2 / 100;
         int total_resto = dia % 100;
         total2 = total_resto % 10;
         total3 = total1 + total2;
-        return total3; // devuelve el mes en 1 cifra
+
+        System.out.println("dia " + dia);
+
+        System.out.println("Primer numero : " + total1);
+        System.out.println("Segundo numero : " + total2);
+        System.out.println("Tercer numero : " + total3);
+
+        int dia22;
+        int total11;
+        int total22;
+        int total33;
+
+        dia22 = total3 * 10;
+        total11 = dia22 / 100;
+        int total_resto2 = total3 % 100;
+        total22 = total_resto2 % 10;
+        total33 = total11 + total22;
+        System.out.println("Valor " + total33);
+        return total33;
     }
 
+    /**
+     * Devuelve el mes en un solo numero
+     *
+     * @param mes
+     * @return
+     */
+    public static int dividir2cifrasMes(int mes) {
+        System.out.println("----------- MES ---------------");
+        int mesDecenas;
+        int mesResto;
+        int mesModulo1;
+        int mesSumaDecenas;
+
+        mesDecenas = mes * 10;
+        mesResto = mesDecenas / 100;
+
+        int mesModulo = mes % 100;
+        mesModulo1 = mesModulo % 10;
+
+        mesSumaDecenas = mesResto + mesModulo1;
+
+        System.out.println("mes : " + mes);
+
+        System.out.println("Primer numero : " + mesResto);
+        System.out.println("Segundo numero : " + mesModulo1);
+        System.out.println("Suma Primer y Segundo : " + mesSumaDecenas);
+
+        int mesUnidades;
+        int mesDivide100;
+        int mesModulo10;
+        int mesUnidadesFinal;
+
+        mesUnidades = mesSumaDecenas * 10;
+        mesDivide100 = mesUnidades / 100;
+        int mesModulo2 = mesSumaDecenas % 100;
+        mesModulo10 = mesModulo2 % 10;
+        mesUnidadesFinal = mesDivide100 + mesModulo10;
+        System.out.println("Valor : " + mesUnidadesFinal);
+        return mesUnidadesFinal;
+    }
+
+    /**
+     * Version arreglada - llega hasta resto 0
+     *
+     * @param valor
+     * @return
+     */
+    public static int dividir2CifrasAnio(int valor) {
+        System.out.println("----------- ANIO ---------------");
+        System.out.println("Anio a dividir :  " + valor);
+        int c1 = valor / 1000;
+        int r1 = valor % 1000;
+        int c2 = r1 / 100;
+        int r2 = r1 % 100;
+        int c3 = r2 / 10;
+        int r3 = r2 % 10;
+        int c4 = r3 / 1;
+        int r4 = r3 % 1;
+        total = c1 + c2 + c3 + c4;
+        int valor2 = total;
+        int c5 = valor2 / 10;
+        int r5 = valor2 % 10;
+        int c6 = r5 / 1;
+        int r6 = r5 % 1;
+        int total2 = c5 + c6;
+        int c7 = total2 / 10;
+        int r7 = total2 % 10;
+        int valor4 = c7 + r7;
+        System.out.println("Valor final : " + valor4);
+        return valor4;
+    }
+
+    /**
+     *
+     * @param dia
+     * @param mes
+     * @param anio
+     * @return
+     */
     public static int sumarTodasFechas(int dia, int mes, int anio) {
         System.out.println("dia : " + dia);
         System.out.println("mes : " + mes);
         System.out.println("anio : " + anio);
+
         int sumaTotal = dia + mes + anio;
-//        int sumaTotal = 29;
+        System.out.println("Suma Total : " + sumaTotal);
 
-        System.out.println("suma total :" + sumaTotal);
-
-        int calculo = sumaTotal * 10;
-        System.out.println("♦ calculo : " + calculo);
-
+        int calculo = sumaTotal * 10; // quita esto
         int divide_calculo = calculo / 100;
-        System.out.println("♦♦ divide_calculo : " + divide_calculo);
-
         int resto_calculo = calculo % 100;
-        System.out.println("resto calculo : " + resto_calculo);
-
         int divide_calculo2 = resto_calculo / 10;
-
-        System.out.println("resto calculo : " + divide_calculo2);
-
         int resto_final = divide_calculo + divide_calculo2;
-
-        System.out.println("resto final : " + resto_final);
-
-        System.out.println("-----------------------------");
-
         int calculo2 = resto_final * 10;
-        System.out.println("calculo2 " + calculo2);
         int divide_calculo_resto = calculo2 / 100;
-        System.out.println("divide_calculo_resto " + divide_calculo_resto);
         int resto_calculo2 = calculo2 % 100;
-        System.out.println("resto_calculo2 : " + resto_calculo2);
         int final_resto = resto_calculo2 / 10;
-        System.out.println("Resto Final : ");
-        System.out.println(final_resto + divide_calculo_resto);
+        int final_resto2 = final_resto + divide_calculo_resto / 10;
+        System.out.println("Valor Final : " + final_resto2);
 
-        return sumaTotal; // 
+        return final_resto2;
     }
 
+    /**
+     *
+     * @return
+     */
     public static int anioActual() {
         Calendar cal = Calendar.getInstance();
         int year = cal.get(Calendar.YEAR);
         return year;
-    }
-
-    public static int totalFecha() {
-        totalFecha = dia + dia + anio;
-        return totalFecha;
     }
 
     public static void main(String[] args) {
@@ -193,22 +225,16 @@ public class T3__20_NumeroDeLaSuerte_Final {
         System.out.println("Dia introducido : " + dia);
         System.out.println("Mes introducido : " + mes);
         System.out.println("Anio introducido : " + anio);
-        System.out.println("Division : " + dividirAnioGeneral(1999));
-        int anioDividido = dividirAnioGeneral(1999);
-        System.out.println("Anio total " + anioDividido);
-
-        System.out.println("Division final : " + dividir2cifrasAnio(anioDividido));
-        int anioResto = dividir2cifrasAnio(anioDividido);
-        System.out.println("Anio total : " + anioResto);
-        System.out.println(".................................");
-
-        int dividir2cifrasMes = dividir2cifrasMes(11);
-        System.out.println("Mes total : " + dividir2cifrasMes);
-
-        int dividir2cifrasDia = dividir2cifrasDia(1);
+        int dividir2cifrasDia = dividir2cifrasDia(dia);
         System.out.println("Dia total : " + dividir2cifrasDia);
+        int dividir2cifrasMes = dividir2cifrasMes(mes);
+        System.out.println("Mes total : " + dividir2cifrasMes);
+        int dividirAnioGeneral = dividir2CifrasAnio(anio);
+        System.out.println("Valor obtenido de la suma del año introducido : " + dividirAnioGeneral);
 
-        System.out.println("---------------------------------");
-        sumarTodasFechas(dividir2cifrasDia, dividir2cifrasMes, anioResto);
+        System.out.println("------------- SALIDA --------------------");
+        int sumarTodasFechas = sumarTodasFechas(dividir2cifrasDia, dividir2cifrasMes, dividirAnioGeneral);
+        System.out.println("--------------------------------");
+        System.out.println("Tu numero de la suerte es el: " + sumarTodasFechas);
     }
 }
