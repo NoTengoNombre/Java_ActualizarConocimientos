7f7592d659664afcefe7d85da66c437f5ee5d136
raulvela <siguelasreglas@gmail.com>
2017-09-26 00:32:38 +0200

    Ejemplo de codigo T5 creados por el profesor

--- Java_Celia/src/java_celia/t5xEjerciciosBasicos/String/T5x25PalindromosProfesor.java
+++ Java_Celia/src/java_celia/t5xEjerciciosBasicos/String/T5x25PalindromosProfesor.java
@@ -13,21 +13,14 @@ import java.io.InputStreamReader;
 
 public class T5x25PalindromosProfesor {
 
-    public static void main(String[] args) {
-
-    }
-}
-
-class PalindromoCharArray {
-
     /**
      * Array de char
      */
     private char[] frase;
 
-    protected int j;
+    private int j;
 
-    PalindromoCharArray() {
+    T5x25PalindromosProfesor() {
         this.j = 0;
     }
 
@@ -37,12 +30,13 @@ class PalindromoCharArray {
     public void leer() {
         String str = null;
         BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Introduce por teclado la frase que quieres cambiar");
         try {
             str = buff.readLine();
         } catch (IOException e) {
-            System.out.println("Excepcion del tipo " + e.getMessage());
-            frase = str.toCharArray();
+            System.out.println("Excepcion del tipo " + e.getMessage() + " Localizacion : " + e.getLocalizedMessage());
         }
+        frase = str.toCharArray();
     }
 
     /**
@@ -57,6 +51,7 @@ class PalindromoCharArray {
                 cont++;
             }
         }
+        System.out.println("Contar caracteres validos " + cont);
         return cont;
     }
 
@@ -65,11 +60,13 @@ class PalindromoCharArray {
      */
     private void quitarEspacios() {
         char[] nuevo = new char[contarCaracteresValidos()];
-        int i;
-        for (i = 0; i < frase.length; i++) {
-            if (frase[i] != ' ') {
-                nuevo[i] = frase[i];
-                j++;
+        int izquierda;
+        int derecha = 0;
+
+        for (izquierda = 0; izquierda < frase.length; izquierda++) {
+            if (frase[izquierda] != ' ') {
+                nuevo[derecha] = frase[izquierda];
+                derecha++;
             }
         }
         frase = nuevo;
@@ -81,12 +78,18 @@ class PalindromoCharArray {
     public void mostrar() {
         int i;
         for (i = 0; i < frase.length; i++) {
-            System.out.println(frase[i]);
+            System.out.print(frase[i]);
         }
         System.out.println();
     }
 
     /**
+     * Comprobamos si la frase es palindroma recorriendola desde el extremo
+     * izquierdo (variable i) y desde el derecho ( variable j )
+     *
+     * Si encontramos alguna pareja diferente , sabremos que la frase no es
+     * palindroma
+     *
      * Recorro el char de array de izquierda (variable i ) a derecha (variable
      * j) Si encontramos alguna pareja diferente , no es palindromo
      *
@@ -94,17 +97,112 @@ class PalindromoCharArray {
      */
     public boolean esPalindromo() {
         int izq = 0;
-        int der = frase.length - 1;
+        int der = (frase.length - 1);
         boolean palindromo = true;
 
-        while (izq < der && palindromo) {
+        while ((izq < der) && palindromo) {
             if (frase[izq] != frase[der]) {
                 palindromo = false;
             }
             izq++;
             der--;
         }
+        esPalindromoSiNo(palindromo);
         return palindromo;
+    }
+
+    public void esPalindromoSiNo(boolean palindromo) {
+        if (palindromo == true) {
+            System.out.println("\u001b[33m La palabra es palindroma");
+        } else {
+            System.out.println("\u001b[35m La palabra es NO palindroma");
+        }
+    }
+
+    /**
+     *
+     */
+    public static void lanzarAppArrayChar() {
+        T5x25PalindromosProfesor p = new T5x25PalindromosProfesor();
+        p.leer();
+        p.quitarEspacios();
+        p.contarCaracteresValidos();
+        p.mostrar();
+        p.esPalindromo();
+    }
+
+}
+
+class PalindromoString {
+
+    private String frase;
+
+    /**
+     *
+     */
+    public void leer() {
+        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Introduce por teclado la frase que quieres cambiar");
+        try {
+            frase = buff.readLine();
+        } catch (IOException e) {
+            System.out.println("Error : " + e.getMessage() + " Localizado : " + e.getLocalizedMessage());
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+    private int contarCaracteresValidos() {
+        return frase.length();
+    }
+
+    /**
+     *
+     */
+    public void quitarEspacios() {
+        frase = frase.replaceAll(" ", "");
+    }
+
+    /**
+     *
+     */
+    public void mostrar() {
+        System.out.println(frase);
+    }
+
+    /**
+     *
+     * @return
+     */
+    public boolean esPalindromo() {
+        StringBuilder str1 = new StringBuilder(frase);
+        String fraseInvertida = (str1.reverse().toString());
+        System.out.println("El valor es : " + frase.equals(fraseInvertida));
+        esPalindromoSiNo(frase.equals(fraseInvertida));
+        return frase.equals(fraseInvertida);
+    }
+
+    public void esPalindromoSiNo(boolean palindromo) {
+        if (palindromo == true) {
+            System.out.println("\u001b[32m Si es palindromo");
+        } else {
+            System.out.println("\u001b[32m No es palindromo");
+        }
+    }
+
+    public static void lanzarAppString() {
+        PalindromoString ps = new PalindromoString();
+        ps.leer();
+        ps.contarCaracteresValidos();
+        ps.quitarEspacios();
+        ps.mostrar();
+        ps.esPalindromo();
+    }
+
+    public static void main(String[] args) {
+        lanzarAppString();
     }
 
 }
